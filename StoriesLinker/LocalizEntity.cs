93dbb5f6a3ab1cee264aa6cafc1cefd53cf6b679
Сергей Шевченко <s.v.shev4enk0@gmail.com>
2025-06-05 09:25:01 +0300

    Remove old file references after reorganization

--- StoriesLinker/LocalizEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StoriesLinker;
-
-public class LocalizEntity
-{
-    public string LocalizID { get; set; } = string.Empty;
-    public string Emotion { get; set; } = string.Empty;
-    public string SpeakerDisplayName { get; set; } = string.Empty;
-}
